ca8927f37f29fdd65d394cd950d34d6a0ccd3c41
Jvaz0715 <james.vazquez@codeimmersives.com>
2021-07-22 18:57:11 -0400

    fixes recipe detail to avoid refresh losing information

--- src/components/Recipe/RecipeDetail.js
+++ src/components/Recipe/RecipeDetail.js
@@ -1,39 +1,40 @@
 import React, { Component } from 'react';
 import axios from "axios";
-
+import "./Recipe.css";
 
 export class RecipeDetail extends Component {
     state = {
-       dishName: "",
-       dishImg: "",
+       dishName: window.sessionStorage.getItem("dishName"),
+       dishImg: window.sessionStorage.getItem("dishImg"),
        recipeID: this.props.location.recipeID, // We set this from our "to" in recipe.js <Link/> we need this id to complete our axios get request
-       recipeURL: "",
-    }
+       recipeURL: window.sessionStorage.getItem("recipeURL"),
+    };
+
     // Why do componentDidMount()? TEST:
-   async componentDidMount() {
-       this.getRecipe();
-   }
+    async componentDidMount(event) {
+        try {
+            event.preventDefault()
+            this.getRecipe();
+        } catch(e){
+            console.log(e)
+       }
+    };
 
-   getRecipe = async () => {
-       try {
+    getRecipe = async () => {
+        try {
              
-             let singleRecipeEndpoint = await axios.get(`https://api.edamam.com/api/recipes/v2/${this.state.recipeID}?type=public&app_id=${process.env.REACT_APP_RECIPE_APPID}&app_key=${process.env.REACT_APP_RECIPE_APIKEY}`);
-             
-
-           
-
-            this.setState({
-                dishName: singleRecipeEndpoint.data.recipe.label,
-                dishImg: singleRecipeEndpoint.data.recipe.image,
-                recipeURL: singleRecipeEndpoint.data.recipe.url,
-            })
-
-            console.log(this.state)
+            let singleRecipeEndpoint = await axios.get(`https://api.edamam.com/api/recipes/v2/${this.state.recipeID}?type=public&app_id=${process.env.REACT_APP_RECIPE_APPID}&app_key=${process.env.REACT_APP_RECIPE_APIKEY}`);
+            
+            window.sessionStorage.setItem("dishName", singleRecipeEndpoint.data.recipe.label)
+            
+            window.sessionStorage.setItem("dishImg", singleRecipeEndpoint.data.recipe.image)
+            
+            window.sessionStorage.setItem("recipeURL", singleRecipeEndpoint.data.recipe.url)
         
         } catch(e) {
            console.log(e)
-       }
-   }
+        }
+    };
 
 
     render() {
@@ -45,7 +46,13 @@ export class RecipeDetail extends Component {
                     <img src={this.state.dishImg} alt={this.state.dishName}/>
                 </div>
                 <div>
+                    <div>
                     <h1>{this.state.dishName}</h1>
+                    <button className="favorite-button">
+                        Add to favorites
+                    </button>
+                    </div>
+                    
                     <a href={this.state.recipeURL} target="_blank" rel="noreferrer">Click Here for recipe</a>
                 </div>
                 

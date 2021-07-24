d7ecf1a0d56d4929216787c5fe4a288a013476ca
Jvaz0715 <james.vazquez@codeimmersives.com>
2021-07-24 14:29:19 -0400

    fixes bug where recipe detail refreshed and emptied. now recipe detail page stays even on refresh. Next will try to get recipe search page to stay the same

--- src/components/Recipe/RecipeDetail.js
+++ src/components/Recipe/RecipeDetail.js
@@ -4,16 +4,16 @@ import "./Recipe.css";
 
 export class RecipeDetail extends Component {
     state = {
-    //    dishName: window.sessionStorage.getItem("dishName"),
-    //    dishImg: window.sessionStorage.getItem("dishImg"),
-       recipeID: this.props.location.recipeID, // We set this from our "to" in recipe.js <Link/> we need this id to complete our axios get request
-    //    recipeURL: window.sessionStorage.getItem("recipeURL"),   
+    dishName: "",
+    dishImg: "",
+    recipeID: this.props.location.recipeID, // We set this from our "to" in recipe.js <Link/> we need this id to complete our axios get request
+    recipeURL: "",   
     };
 
     // Why do componentDidMount()? TEST:
-    componentDidMount(event) {
+    async componentDidMount() {
         try {
-            this.getRecipe();
+            await this.getRecipe();
             
         } catch(e){
             console.log(e)
@@ -24,18 +24,18 @@ export class RecipeDetail extends Component {
         try {
              
             let singleRecipeEndpoint = await axios.get(`https://api.edamam.com/api/recipes/v2/${this.state.recipeID}?type=public&app_id=${process.env.REACT_APP_RECIPE_APPID}&app_key=${process.env.REACT_APP_RECIPE_APIKEY}`);
+            window.localStorage.setItem("dishName", singleRecipeEndpoint.data.recipe.label)
             
-            // this.setState({
-            //     dishName: singleRecipeEndpoint.data.recipe.label,
-            //     dishImg: singleRecipeEndpoint.data.recipe.image,
-            //     recipeURL: singleRecipeEndpoint.data.recipe.url,
-            // });
+            window.localStorage.setItem("dishImg", singleRecipeEndpoint.data.recipe.image)
+            
+            window.localStorage.setItem("recipeURL", singleRecipeEndpoint.data.recipe.url)
+            this.setState({
+                dishName: singleRecipeEndpoint.data.recipe.label,
+                dishImg: singleRecipeEndpoint.data.recipe.image,
+                recipeURL: singleRecipeEndpoint.data.recipe.url,
+            });
 
-            window.sessionStorage.setItem("dishName", singleRecipeEndpoint.data.recipe.label)
             
-            window.sessionStorage.setItem("dishImg", singleRecipeEndpoint.data.recipe.image)
-            
-            window.sessionStorage.setItem("recipeURL", singleRecipeEndpoint.data.recipe.url)
         
         } catch(e) {
            console.log(e)
@@ -49,17 +49,17 @@ export class RecipeDetail extends Component {
             
             <div style={{display: "flex", flexDirection:"column"}}>
                 <div>
-                    <img src={window.sessionStorage.getItem("dishImg")} alt={window.sessionStorage.getItem("dishName")}/>
+                    <img src={window.localStorage.getItem("dishImg")} alt={window.localStorage.getItem("dishName")}/>
                 </div>
                 <div>
                     <div>
-                    <h1>{window.sessionStorage.getItem("dishName")}</h1>
+                    <h1>{window.localStorage.getItem("dishName")}</h1>
                     <button className="favorite-button">
                         Add to favorites
                     </button>
                     </div>
                     
-                    <a href={window.sessionStorage.getItem("recipeURL")} target="_blank" rel="noreferrer">Click Here for recipe</a>
+                    <a href={window.localStorage.getItem("recipeURL")} target="_blank" rel="noreferrer">Click Here for recipe</a>
                 </div>
                 
     

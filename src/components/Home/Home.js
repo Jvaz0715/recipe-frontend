c61a4aaf137464efaff6fa7eb7bc3ce5fd9a6e48
Jvaz0715 <james.vazquez@codeimmersives.com>
2021-07-24 12:13:26 -0400

    Adds styling to home page...

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,21 @@
 import React, { Component } from "react";
+import "./Home.css";
 
 export class Home extends Component {
   render() {
     return (
-      <div style={{ textAlign: "center", marginTop: "15%", fontSize: 75 }}>
-        Welcome to Coolest APP
+      <div className="home-container">
+        <div className="home-body-div">
+          <div className="home-images-div">
+            <img src="https://www.freepnglogos.com/uploads/chef-png/chef-sitar-knoxville-authentic-indian-restaurant-29.png" alt="cartoon-chef"/>
+          </div>
+          <div className="home-app-description-div">
+            <div>
+              
+            </div>
+          </div>
+
+        </div>
       </div>
     );
   }
--- src/components/Recipe/RecipeDetail.js
+++ src/components/Recipe/RecipeDetail.js
@@ -4,17 +4,17 @@ import "./Recipe.css";
 
 export class RecipeDetail extends Component {
     state = {
-       dishName: window.sessionStorage.getItem("dishName"),
-       dishImg: window.sessionStorage.getItem("dishImg"),
+    //    dishName: window.sessionStorage.getItem("dishName"),
+    //    dishImg: window.sessionStorage.getItem("dishImg"),
        recipeID: this.props.location.recipeID, // We set this from our "to" in recipe.js <Link/> we need this id to complete our axios get request
-       recipeURL: window.sessionStorage.getItem("recipeURL"),
+    //    recipeURL: window.sessionStorage.getItem("recipeURL"),   
     };
 
     // Why do componentDidMount()? TEST:
-    async componentDidMount(event) {
+    componentDidMount(event) {
         try {
-            event.preventDefault()
             this.getRecipe();
+            
         } catch(e){
             console.log(e)
        }
@@ -25,6 +25,12 @@ export class RecipeDetail extends Component {
              
             let singleRecipeEndpoint = await axios.get(`https://api.edamam.com/api/recipes/v2/${this.state.recipeID}?type=public&app_id=${process.env.REACT_APP_RECIPE_APPID}&app_key=${process.env.REACT_APP_RECIPE_APIKEY}`);
             
+            // this.setState({
+            //     dishName: singleRecipeEndpoint.data.recipe.label,
+            //     dishImg: singleRecipeEndpoint.data.recipe.image,
+            //     recipeURL: singleRecipeEndpoint.data.recipe.url,
+            // });
+
             window.sessionStorage.setItem("dishName", singleRecipeEndpoint.data.recipe.label)
             
             window.sessionStorage.setItem("dishImg", singleRecipeEndpoint.data.recipe.image)
@@ -43,17 +49,17 @@ export class RecipeDetail extends Component {
             
             <div style={{display: "flex", flexDirection:"column"}}>
                 <div>
-                    <img src={this.state.dishImg} alt={this.state.dishName}/>
+                    <img src={window.sessionStorage.getItem("dishImg")} alt={window.sessionStorage.getItem("dishName")}/>
                 </div>
                 <div>
                     <div>
-                    <h1>{this.state.dishName}</h1>
+                    <h1>{window.sessionStorage.getItem("dishName")}</h1>
                     <button className="favorite-button">
                         Add to favorites
                     </button>
                     </div>
                     
-                    <a href={this.state.recipeURL} target="_blank" rel="noreferrer">Click Here for recipe</a>
+                    <a href={window.sessionStorage.getItem("recipeURL")} target="_blank" rel="noreferrer">Click Here for recipe</a>
                 </div>
                 
     

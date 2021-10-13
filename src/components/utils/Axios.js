ff66612156e837b2d0d67a6daad146f021b4af49
Jvaz0715 <James.Vazquez@codeimmersives.com>
2021-10-13 16:02:44 -0400

    makes changes reflecting aws tutorial

--- src/components/utils/Axios.js
+++ src/components/utils/Axios.js
@@ -2,9 +2,9 @@ import axios from "axios";
 
 const Axios = axios.create({
   baseURL:
-    process.env.NODE_ENV === "development"
-      ? "http://localhost:8080"
-      : "deploy CLOUD ADDRESS",
+    process.env.REACT_APP_AXIOS === "development"
+      ? "http://localhost:3001"
+      : "/",
   timeout: 50000,
 });
 
